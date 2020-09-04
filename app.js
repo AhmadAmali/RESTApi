1efa2e2c601a791ba2411ccaa9f3fd064e60da65
AhmadAbroad22 <ahmadamali77@gmail.com>
2020-09-03 20:10:15 -0600

    connected to database, created skeleton schema

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+
+//importing routes
+const postRoute = require('./routes/home');
+
+app.use('/home', postRoute);
 
 app.use('/', () => {
     console.log("this works innit");
 });
 
 
-app.get('/',(req,res) => {
-    res.send('We are on home');
 
-}); 
+
+// establishing db connection
+mongoose.connect
+(process.env.DB_CONNECTION, 
+{ useNewUrlParser: true },
+{ useUnifiedTopology: true }, 
+() => console.log('connect to db')
+);
 app.listen(3000);
