58cf8f2191508855d3169d2a8b685b40f930499a
AhmadAbroad22 <ahmadamali77@gmail.com>
2020-09-20 01:23:20 -0600

    added new package for external API usage and added router modules for manipulating data schemas

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
+
+//middlewars
+app.use(cors());
+app.use(bodyParser.json());
 
 
 //importing routes
